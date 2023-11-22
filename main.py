36d4cafedd0d1e6f2e42cc8e0a1c1eeb3d11c6e9
Anton <an88ton@gmail.com>
2023-11-22 15:16:49 +0200

    improved creation masks, readme added

--- main.py
+++ main.py
@@ -8,45 +8,29 @@ import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
 
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 BUFFER_SIZE = 1000
-STEPS_PER_EPOCH = 4
-RESERVED_FOR_TRAIN = 2
+STEPS_PER_EPOCH = 120
+RESERVED_FOR_TRAIN = 2000
 
 
-image_folder_path  = './airbus-ship-detection/train/train_test/images/'
-mask_folder_path  = './airbus-ship-detection/train/train_test/masks/'
-
-img_ds = tf.keras.utils.image_dataset_from_directory(image_folder_path, labels=None, shuffle=None)
-
-segmentation_mask_ds = tf.keras.utils.image_dataset_from_directory(mask_folder_path, labels=None, shuffle=None, color_mode='grayscale')  # Assumes the filenames are in the same order
-ds = tf.data.Dataset.zip((img_ds, segmentation_mask_ds))
+image_folder_path  = './airbus-ship-detection/train_v2'
+mask_folder_path  = './airbus-ship-detection/masks_v2'
 
 def normalize(input_image, input_mask):
   input_image = tf.cast(input_image, tf.float32) / 255.0
   input_mask = tf.cast(input_mask, tf.float32) / 255.0
-  # print(input_mask[0])
-  # input_mask -= 1
   return input_image, input_mask
 
 def load_image(img, mask):
-
-  input_image = tf.image.resize(img, (128, 128))
-  print('21', input_image)
-
+  input_image = tf.image.resize(img, (256, 256))
   input_mask = tf.image.resize(
     mask,
-    (128, 128),
+    (256, 256),
     method = tf.image.ResizeMethod.NEAREST_NEIGHBOR,
   )
-
   input_image, input_mask = normalize(input_image, input_mask)
-
   return input_image, input_mask
-
-ds.batch(BATCH_SIZE)
-train_images = ds.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-
 
 class Augment(tf.keras.layers.Layer):
     def __init__(self, seed=42):
@@ -60,19 +44,6 @@ class Augment(tf.keras.layers.Layer):
         labels = self.augment_labels(labels)
         return inputs, labels
 
-train_batches = (
-    train_images
-    .cache()
-    .shuffle(BUFFER_SIZE)
-    # .batch(BATCH_SIZE)
-    .repeat()
-    .map(Augment())
-    .prefetch(buffer_size=tf.data.AUTOTUNE)
-    )
-
-test_batches = train_batches.take(RESERVED_FOR_TRAIN)
-train_batches = train_batches.skip(RESERVED_FOR_TRAIN)
-
 def display(display_list):
   plt.figure(figsize=(15, 15))
 
@@ -85,13 +56,39 @@ def display(display_list):
     plt.axis('off')
   plt.show()
 
+# Loading images and masks
+img_ds = tf.keras.utils.image_dataset_from_directory(image_folder_path, labels=None, shuffle=None)
+segmentation_mask_ds = tf.keras.utils.image_dataset_from_directory(mask_folder_path, labels=None, shuffle=None, color_mode='grayscale')  # Assumes the filenames are in the same order
+# Combine to one dataset, data will be in tuples (img, mask)
+ds = tf.data.Dataset.zip((img_ds, segmentation_mask_ds))
+
+ds.batch(BATCH_SIZE)
+train_images = ds.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
+
+train_batches = (
+    train_images
+    .cache()
+    # .shuffle(BUFFER_SIZE)
+    # .batch(BATCH_SIZE) batch was setted above, if we call it here it will breake shape of dataset
+    .repeat()
+    .map(Augment())
+    .prefetch(buffer_size=tf.data.AUTOTUNE)
+    )
+
+# Split dataset because original data for test was corrupted
+test_batches = train_batches.take(RESERVED_FOR_TRAIN).cache()
+train_batches = train_batches.skip(RESERVED_FOR_TRAIN)
+
+
+
 for images, masks in train_batches.take(2):
   sample_image, sample_mask = images[0], masks[0]
   display([sample_image, sample_mask])
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+# Train model
+base_model = tf.keras.applications.MobileNetV2(input_shape=[256, 256, 3], include_top=False)
 
-# Use the activations of these layers
+
 layer_names = [
     'block_1_expand_relu',   # 64x64
     'block_3_expand_relu',   # 32x32
@@ -114,7 +111,7 @@ up_stack = [
 ]
 
 def unet_model(output_channels:int):
-  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+  inputs = tf.keras.layers.Input(shape=[256, 256, 3])
 
   # Downsampling through the model
   skips = down_stack(inputs)
@@ -139,7 +136,7 @@ def unet_model(output_channels:int):
 OUTPUT_CLASSES = 3
 
 model = unet_model(output_channels=OUTPUT_CLASSES)
-model.compile(optimizer='adam',
+model.compile(optimizer= 'adam', #tf.keras.optimizers.Adam(learning_rate=0.01),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
@@ -163,14 +160,49 @@ def show_predictions(dataset=None, num=1):
 class DisplayCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs=None):
     clear_output(wait=True)
-    # show_predictions()
+    show_predictions()
     print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
 
 EPOCHS = 20
-VAL_SUBSPLITS = 5
 
 model_history = model.fit(train_batches, epochs=EPOCHS,
                           steps_per_epoch=STEPS_PER_EPOCH,
                           validation_data=test_batches,
                           callbacks=[DisplayCallback()])
 
+# label = [0,0]
+# prediction = [[-3., 0], [-3, 0]]
+# sample_weight = [1, 10]
+#
+# loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,
+#                                                reduction=tf.keras.losses.Reduction.NONE)
+# loss(label, prediction, sample_weight).numpy()
+#
+# def add_sample_weights(image, label):
+#   # The weights for each class, with the constraint that:
+#
+#   class_weights = tf.constant([3.0, 1.0, 1.0])
+#   # print(sum(class_weights) == 1.0)
+#   class_weights = class_weights/tf.reduce_sum(class_weights)
+#
+#   # Create an image of `sample_weights` by using the label at each pixel as an
+#   # index into the `class weights` .
+#   sample_weights = tf.gather(class_weights, indices=tf.cast(label, tf.int32))
+#
+#   return image, label, sample_weights
+#
+# train_batches.map(add_sample_weights).element_spec
+#
+# weighted_model = unet_model(OUTPUT_CLASSES)
+# weighted_model.compile(
+#     optimizer='adam',
+#     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#     metrics=['accuracy'])
+#
+# weighted_model.fit(
+#     train_batches.map(add_sample_weights),
+#     epochs=10,
+#     steps_per_epoch=10,
+#     validation_data=test_batches,
+#     callbacks=[DisplayCallback()]
+# )
